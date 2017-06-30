05662aa2b112abd494166746f3c0da422f7f0ee4
timrooke1991 <timrooke1991@gmail.com>
2017-06-30 15:46:20 +0100

    Mocha tests created for RESTful routes

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,5 @@
 const port    = process.env.PORT || 4000;
 const env     = process.env.NODE_ENV || 'development';
+const secret = process.env.SECRET || 'shhhh...it is a secret!';
 
-module.exports = { port, env };
+module.exports = { port, env, secret };
---
+++ test/api/auth_test.js
@@ -0,0 +1,89 @@
+/* globals: api */
+require('../helper');
+
+const User = require('../../models/user');
+
+describe('Authentications tests', () => {
+
+  beforeEach((done) => {
+    User.collection.drop();
+    done();
+  });
+
+  xdescribe('POST /api/register with good credentials', () => {
+    it('should return a message', (done) => {
+      api.post('/api/register')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'test',
+          email: 'test@test.com',
+          password: 'password',
+          passwordConfirmation: 'password'
+        })
+        .end((err, res) => {
+          expect(res.body.message).to.be.a('string');
+          done();
+        });
+    });
+  });
+
+  describe('POST /api/register with bad credentials', () => {
+    it('should return a 400 response', (done) => {
+      api.post('/api/register')
+      .set('Accept', 'application/json')
+      .send({
+        username: 'test',
+        email: 'test@test.com'
+      })
+      .expect(400, done);
+    });
+  });
+
+  describe('POST /api/login with good credentials', () => {
+
+    beforeEach((done) => {
+      User.create({
+        username: 'test',
+        email: 'test@test.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }, done);
+    });
+
+    it('should return a token', (done) => {
+      api.post('/api/login')
+        .set('Accept', 'application/json')
+        .send({
+          email: 'test@test.com',
+          password: 'password'
+        })
+        .end((err, res) => {
+          expect(res.body.token).to.be.a('string');
+          done();
+        });
+    });
+  });
+
+  describe('POST /api/login with bad credentials', () => {
+
+    beforeEach((done) => {
+      User.create({
+        username: 'test',
+        email: 'test@test.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }, done);
+    });
+
+    it('should return a 401 response', (done) => {
+      api.post('/api/login')
+        .set('Accept', 'application/json')
+        .send({
+          email: 'test@test.com',
+          password: 'fail'
+        })
+        .expect(401, done);
+    });
+  });
+
+});

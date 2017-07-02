9ff5d355b445a69d2557ca1b3ec009af13c3a913
Chisomo Lungu <chisomo@gmail.com>
2017-07-02 16:22:09 +0100

    forms ng validated

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,3 @@
 angular
-  .module('disasterRelief', ['ngResource', 'ui.router', 'ngMessages', 'satellizer']);
+  .module('disasterRelief', ['ngResource', 'ui.router', 'ngMessages', 'satellizer', 'ngPassword']);
+  //'ngPassword' is used to check the registeration form to match the passwords

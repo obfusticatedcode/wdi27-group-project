73926e654dd2111402bbae2c92f4b768273bb205
Chisomo Lungu <chisomo@gmail.com>
2017-07-03 13:05:53 +0100

    about to merge

--- src/js/services/getDistance.js
+++ src/js/services/getDistance.js
@@ -1,15 +1,14 @@
+/*global geolib*/
 angular
   .module('disasterRelief')
   .service('GetDistance', GetDistance );
 
-
-
 function GetDistance(geolib){
 
   return geolib.getDistance(
-    {latitude: 51, longitude: 7.49347},
-    {latitude: 51, longitude: 7.49347}
+    {lat: 51, lng: 7.49347},
+    {lat: 51, lng: 7.49347}
   );
 }
 
-console.log(GetDistance(geolib));
+console.log('Service output', GetDistance(geolib));

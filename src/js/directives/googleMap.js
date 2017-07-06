7b06d49453c6a6cb9ad212c9f5d2396263e620ef
Chisomo Lungu <chisomo@gmail.com>
2017-07-06 11:31:56 +0100

    fixed the null map issue

--- src/js/directives/googleMap.js
+++ src/js/directives/googleMap.js
@@ -17,9 +17,11 @@ function googleMap() {
       let map = null;
       let marker = null;
 
+
       scope.$watch('locations', generateMarkers);
       scope.$on('$destroy', destroyMap);
       scope.$watch('center', centerMap);
+      // console.log(element[0]);
 
       map = new google.maps.Map(element[0], {
         zoom: 8,
@@ -31,11 +33,10 @@ function googleMap() {
         if(!center) return false;
         map.setCenter(center);
         map.setZoom(3);
-
       }
 
       function generateMarkers(locations) {
-
+        if(!scope.locations) return false;
         // Create an array of alphabetical characters used to label the markers.
         const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
@@ -43,15 +44,15 @@ function googleMap() {
         // Note: The code uses the JavaScript Array.prototype.map() method to
         // create an array of markers based on a given "locations" array.
         // The map() method here has nothing to do with the Google Maps API.
+
         const markers = scope.locations.map(function(location, i) {
+
           return new google.maps.Marker({
             position: location,
             label: labels[i % labels.length]
           });
-        });
 
-        console.log('Locations: ', locations);
-        console.log('Markers: ', markers);
+        });
 
         // Add a marker clusterer to manage the markers.
         new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });

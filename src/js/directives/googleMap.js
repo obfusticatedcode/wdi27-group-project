054140e99b752386f1a2b249f1eb96078e11f821
Chisomo Lungu <chisomo@gmail.com>
2017-07-06 14:12:22 +0100

    added info window

--- src/js/directives/googleMap.js
+++ src/js/directives/googleMap.js
@@ -35,7 +35,12 @@ function googleMap() {
         map.setZoom(3);
       }
 
-      function generateMarkers(locations) {
+      function alert() {
+        alert('Function called');
+      }
+
+
+      function generateMarkers() {
         if(!scope.locations) return false;
         // Create an array of alphabetical characters used to label the markers.
         const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -52,6 +57,39 @@ function googleMap() {
             label: labels[i % labels.length]
           });
 
+        });
+
+        markers.forEach((marker) => {
+          var contentString = '<div id="content">'+
+          '<div id="siteNotice">'+
+          '</div>'+
+          '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+          '<div id="bodyContent">'+
+          '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+          'sandstone rock formation in the southern part of the '+
+          'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+          'south west of the nearest large town, Alice Springs; 450&#160;km '+
+          '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+          'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+          'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+          'Aboriginal people of the area. It has many springs, waterholes, '+
+          'rock caves and ancient paintings. Uluru is listed as a World '+
+          'Heritage Site.</p>'+
+          '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+          'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+          '(last visited June 22, 2009).</p>'+
+          '</div>'+
+          '</div>';
+
+          var infowindow = new google.maps.InfoWindow({
+            content: contentString
+          });
+
+          google.maps.event.addListener(marker, 'click', function () {
+            // When clicked, open the selected marker's message
+            console.log('I have been clicked!');
+            infowindow.open(map, marker);
+          });
         });
 
         // Add a marker clusterer to manage the markers.

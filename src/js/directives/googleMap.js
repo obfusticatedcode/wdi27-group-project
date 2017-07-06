6cf0ddbe602ccae67bd69d7a0643c4037a185390
Chisomo Lungu <chisomo@gmail.com>
2017-07-06 14:59:14 +0100

    info window now displays the campaign information

--- src/js/directives/googleMap.js
+++ src/js/directives/googleMap.js
@@ -11,17 +11,18 @@ function googleMap() {
     template: '<div class="map">Google Map</div>',
     scope: {
       center: '=',
-      locations: '='
+      locations: '=',
+      campaigns: '='
     },
     link(scope, element) {
       let map = null;
       let markers = null;
 
-
       scope.$watch('locations', generateMarkers);
       scope.$on('$destroy', destroyMap);
       scope.$watch('center', centerMap);
-      // console.log(element[0]);
+
+
 
       map = new google.maps.Map(element[0], {
         zoom: 8,
@@ -35,13 +36,11 @@ function googleMap() {
         map.setZoom(3);
       }
 
-      function alert() {
-        alert('Function called');
-      }
-
 
       function generateMarkers() {
         if(!scope.locations) return false;
+
+        console.log('Campaigns: ', scope.campaigns);
         // Create an array of alphabetical characters used to label the markers.
         const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
@@ -59,38 +58,22 @@ function googleMap() {
 
         });
 
-        markers.forEach((marker) => {
-          var contentString = '<div id="content">'+
-          '<div id="siteNotice">'+
-          '</div>'+
-          '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-          '<div id="bodyContent">'+
-          '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-          'sandstone rock formation in the southern part of the '+
-          'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-          'south west of the nearest large town, Alice Springs; 450&#160;km '+
-          '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-          'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-          'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-          'Aboriginal people of the area. It has many springs, waterholes, '+
-          'rock caves and ancient paintings. Uluru is listed as a World '+
-          'Heritage Site.</p>'+
-          '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-          'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-          '(last visited June 22, 2009).</p>'+
-          '</div>'+
-          '</div>';
 
-          var infowindow = new google.maps.InfoWindow({
-            content: contentString
-          });
+        const infowindow = new google.maps.InfoWindow();
+        for(let i = 0; i < markers.length; i++) {
+          const contentString = `
+            <strong>${scope.campaigns[i].name}</strong>
+            <p>${scope.campaigns[i].description}</p>
+            <p>${scope.campaigns[i].distance} km away</p>
+            `;
 
-          google.maps.event.addListener(marker, 'click', function () {
+          google.maps.event.addListener(markers[i], 'click', function () {
             // When clicked, open the selected marker's message
             console.log('I have been clicked!');
-            infowindow.open(map, marker);
+            infowindow.setContent(contentString);
+            infowindow.open(map, this);
           });
-        });
+        }
 
         // Add a marker clusterer to manage the markers.
         new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
